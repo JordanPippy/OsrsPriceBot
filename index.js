11d831539cec759c6feeac09d8fa06a66a339eec
Jordan <136734p@acadiau.ca>
2021-01-13 16:20:50 -0400

    gets price

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+var request = require("request");
+var fs = require("fs");
 const bot = new Discord.Client();
 
 const TOKEN = process.env.TOKEN;
-
+const linkHead = "https://oldschool.runescape.wiki/w/";
 bot.login(TOKEN);
 
 bot.on('ready', () => {
@@ -13,12 +15,15 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
     //Check if message was for the bot.
+    let message = "";
     if (msg.content[0] === '#') {
-        let message = msg.content.split(' ');
+        message = msg.content.split(' ');
         if (message[0] !== "#pc")
             return;
     }
-    
+    else 
+        return;
+
     //After this point, the message was recognized, bot is working.
     let item = "";
     for (let i = 1; i < message.length; i++) {  
@@ -26,6 +31,34 @@ bot.on('message', msg => {
             item += ' ';
         item += message[i];
     }
+    visitLink(item);
     console.log(item);
     msg.channel.send('pong');
 });
+
+function createLink(link) {
+    let fullLink = link.replace(/ /g, '_');
+    fullLink = linkHead + fullLink;
+    return fullLink;
+}
+
+function visitLink(link) {
+    let fullLink = createLink(link);
+    let price = "";
+    request({uri: fullLink}, 
+    function(error, response, body) {
+        let index = body.indexOf("data-val-each");
+        while (true)
+        {
+            let c = body.charAt(index);
+            if (c != '>')
+                price += c;
+            else 
+                break;
+            index++;
+        }
+        price = price.split('=');
+        price = price[1].substring(1, price[1].length - 1);
+        console.log(price);
+    });
+}
